6f0e4352b2ef228e94e3bda7ea88db228db1a0a5
zidanali111 <zidanali111@gmail.com>
2025-02-07 12:07:24 +0530

    dependencies and inital code added

---
+++ src/test/java/com/payvault/backend/PayvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payvault.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
