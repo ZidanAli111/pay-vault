c376c64e29246e75313b9d9a9025d78f12b67d06
zidanali111 <zidanali111@gmail.com>
2025-03-31 21:14:11 +0530

    Admin controller added

---
+++ src/main/java/com/payvault/backend/auth/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.payvault.backend.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/dashboard")
+    public String getAdminDashboard() {
+        return "Welcome to the admin Dashboard!";
+    }
+}
