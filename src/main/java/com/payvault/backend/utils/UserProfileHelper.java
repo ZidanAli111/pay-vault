2299af08c38b0225a4cbf5b7b8fe71f1031e3185
zidanali111 <zidanali111@gmail.com>
2025-05-10 17:26:53 +0530

    country to userPreference mapping

---
+++ src/main/java/com/payvault/backend/utils/UserProfileHelper.java
@@ -0,0 +1,43 @@
+package com.payvault.backend.utils;
+
+import com.payvault.backend.dto.UserProfileResponse;
+import com.payvault.backend.entity.UserProfile;
+
+import java.util.Currency;
+import java.util.Locale;
+
+
+public class UserProfileHelper {
+
+    public UserProfileResponse mapToUserProfileResponse(UserProfile savedUser) {
+
+        return UserProfileResponse.builder()
+                .userId(savedUser.getUserId())
+                .name(savedUser.getName())
+                .email(savedUser.getEmail())
+                .username(savedUser.getUsername())
+                .password(savedUser.getPassword())
+                .bio(savedUser.getBio())
+                .profilePictureUrl(savedUser.getProfilePictureUrl())
+                .role(savedUser.getRole())
+                .build();
+
+    }
+
+    public String mapCountryToUserPreference(String country) {
+        if (country == null || country.isBlank()) return "USD";
+
+        for (Locale locale : Locale.getAvailableLocales()) {
+            if (locale.getDisplayCountry().equalsIgnoreCase(country.trim())) {
+                try {
+                    Currency currency = Currency.getInstance(locale);
+                    return currency.getCurrencyCode().toUpperCase();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "USD";
+    }
+
+}
