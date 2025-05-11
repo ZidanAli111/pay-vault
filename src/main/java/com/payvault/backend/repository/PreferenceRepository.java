8ff2e400d83cc0e112ca2c2749d2ca700085eff8
zidanali111 <zidanali111@gmail.com>
2025-05-11 12:51:58 +0530

    PreferenceRepository getProfile method added

--- src/main/java/com/payvault/backend/repository/PreferenceRepository.java
+++ src/main/java/com/payvault/backend/repository/PreferenceRepository.java
@@ -11,4 +11,6 @@ import java.util.UUID;
 public interface PreferenceRepository extends JpaRepository<UserPreference, UUID> {
 
     boolean existsByUserProfile(UserProfile userProfile);
+
+    UserPreference findByUserProfile(UserProfile userProfile);
 }
