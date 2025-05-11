34b2eb7c27e6fe6246e32c2078b7fa292646bb3a
zidanali111 <zidanali111@gmail.com>
2025-05-11 12:51:48 +0530

    AddressRepository getProfile method added

--- src/main/java/com/payvault/backend/repository/AddressRepository.java
+++ src/main/java/com/payvault/backend/repository/AddressRepository.java
@@ -1,6 +1,7 @@
 package com.payvault.backend.repository;
 
 import com.payvault.backend.entity.Address;
+import com.payvault.backend.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.UUID;
 @Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
 
+    Address findByUserProfile(UserProfile userProfile);
 }
